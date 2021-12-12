8d337105709682ec3b782feadd1705f70f184196
micmus4 <micmus4@st.amu.edu.pl>
2021-12-12 22:33:32 +0100

    TG-23 - model extensions.

---
+++ app/src/main/java/model/extensions/AnnouncementIf.java
@@ -0,0 +1,14 @@
+package model.extensions;
+
+/**
+ * Interface for {@link Announcement}
+ *
+ * @author created: Michał Musiałowicz on 12.12.2021
+ * @author last changed:
+ */
+public interface AnnouncementIf
+{
+    int getLecturerId();
+
+    String getDetails();
+}
---
+++ app/src/main/java/model/extensions/TimeTable.java
@@ -0,0 +1,37 @@
+package model.extensions;
+
+import model.StudyGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Implementation for TimeTable.
+ *
+ * @author created: Michał Musiałowicz on 12.12.2021
+ * @author last changed:
+ */
+public class TimeTable implements TimeTableIf
+{
+    private final int studentId;
+
+    private final List< StudyGroup > studyGroups;
+
+    public TimeTable( int aStudentId )
+    {
+        studentId = aStudentId;
+        studyGroups = new ArrayList<>();
+    }
+
+    @Override
+    public int getStudentId()
+    {
+        return studentId;
+    }
+
+    @Override
+    public List< StudyGroup > getStudyGroups()
+    {
+        return studyGroups;
+    }
+}
