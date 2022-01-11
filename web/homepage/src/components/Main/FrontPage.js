e5fb5ead194dfa9d02c4363e85937d416989e06c
Mateusz Cwojdzinski <45481835+mcwojdzinski@users.noreply.github.com>
2022-01-11 15:27:10 +0100

    TG-30 - Add homepage site in React (#31)
    * Web files
    
    * Delete mobilki.png
    
    * [TG-8] Add project generated project files via React-CLI
    
    * change directory of html_css files
    
    * css fix
    
    * TG-30 - Restructure plain html and css to React for homepage
    
    Co-authored-by: xcaho <szymon_mazurek@outlook.com>
    Co-authored-by: Mateusz Cwojdziński <mcwojdzinski@Macbook.local>
    Co-authored-by: happy531 <jedrzejwesolowski177@gmail.com>

---
+++ web/homepage/src/components/Main/FrontPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const FrontPage = () => {
+  return (
+    <section className="front-page" id="front-page">
+      <h2>
+        Intuicyjny i <br /> rozbudowany <br /> dziennik elektroniczny
+      </h2>
+      <p>Inżynieria oprogramowania - projekt zaliczeniowy</p>
+      <a href="#" className="btn">
+        Dokumentacja
+      </a>
+    </section>
+  );
+};
+
+export default FrontPage;
