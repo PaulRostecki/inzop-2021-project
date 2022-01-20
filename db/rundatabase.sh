062e60477b48056711831a7e19af0fb01ea7d0f5
PaulRostecki <rostecki.p@gmail.com>
2022-01-20 22:35:47 +0100

    TG-25 fix problems with docker script

--- db/rundatabase.sh
+++ db/rundatabase.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip3 install -r requirements.txt > /dev/null
+pip install -r requirements.txt > /dev/null #if doesn't work, replace pip with pip3
 docker run -d \
   --name postgresql \
   -e POSTGRES_PASSWORD=$(awk -F "=" '/pass/ {print $2}' dbconfig.ini | sed 's/ //g') \
@@ -8,6 +8,6 @@ docker run -d \
   -e POSTGRES_DB=$(awk -F "=" '/db/ {print $2}' dbconfig.ini | sed 's/ //g') \
   -p $(awk -F "=" '/port/ {print $2}' dbconfig.ini | sed 's/ //g'):5432 \
   postgres:14.1
-sleep 1 # database needs some time to set up
-python3 import_demo.py
+sleep 10 # database needs some time to set up
+python import_demo.py #if doesn't work, replace python with python3
 echo "The database is ready!"
